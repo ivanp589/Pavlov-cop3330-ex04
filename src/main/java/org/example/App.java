5d327d423d3a99fea2c00d46bb892b27ce828e60
ivanp589 <ivanpav@Knights.ucf.edu>
2021-09-07 22:34:21 -0400

    copyright update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,8 @@ package org.example;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  *  Copyright 2021 ivan pavlov
  */
 public class App 
 {
